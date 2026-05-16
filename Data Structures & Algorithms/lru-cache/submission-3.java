fd7554f947f98cd841fdb3d5ea0ac958fd674365
Shaswat Gupta <rishugupta2001.rg@gmail.com>
2026-05-16 23:16:03 +0530

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.java
@@ -0,0 +1,20 @@
+class LRUCache {
+    private final int capacity ;
+    private final LinkedHashMap<Integer,Integer> map;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new LinkedHashMap<Integer, Integer> (capacity, 0.75f, true) {
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
+                return size() > LRUCache.this.capacity;
+            }
+        };
+    }
+    
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
