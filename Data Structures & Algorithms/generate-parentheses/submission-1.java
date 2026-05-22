e4b2b57edbb2a4f554a7a832d5f0b1b17a70fc15
Shaswat Gupta <rishugupta2001.rg@gmail.com>
2026-05-22 18:13:27 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        dfs(n, "", 0,0,ans);
+        return ans;
+    }
+
+    public void dfs(int n, String tmp, int open, int close, List<String> ans) {
+        if(open < close || open+close >2*n) {
+            return;
+        }
+
+        if(open + close == n*2 && open == close) {
+            ans.add(tmp);
+            return;
+        }
+
+        String tmp1 = tmp + '(';
+        dfs(n , tmp1, open+1, close, ans);
+        tmp1 = tmp + ')' ;
+        dfs(n , tmp1, open, close+1, ans);
+        return;
+    }
+}
